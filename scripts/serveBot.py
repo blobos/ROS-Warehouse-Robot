31c5aba02d4b1513693b9a8e30a0b365a1882339
blobos <vxc.shirou@gmail.com>
2022-07-31 20:53:57 +0800

    Update code with code from Color(small map demo)

--- scripts/serveBot.py
+++ scripts/serveBot.py
@@ -18,12 +18,9 @@ waypoints = [
     [(5.5, 2.3, 0.0), (0.0, 0.0, 0, 1.0)],
     # 5 Charging Station
     [(7.8, -8.4, 0.0), (0.0, 0.0, -1, 0.0)],
-    # 6 Garbage/Return/Drink Refill
-    [(4, -9.3, 0.0), (0.0, 0.0, 0.7, 0.7)],
+    # # 6 Garbage/Return/Drink Refill
+    # [(4, -9.3, 0.0), (0.0, 0.0, 0.7, 0.7)],
     ]
-
-client = None
-solicit_bool = False
 
 def goal_pose(pose):
     goal_pose = MoveBaseGoal()
@@ -38,92 +35,191 @@ def goal_pose(pose):
 
     return goal_pose
 
+client = None
+solicit_bool = False
+# confirm_bool = False #for remote confirm
+change_bool = False
+prev_cmd = "None"
+state_label = 'default'
+solicit_message = "Come and try our new dessert"
+
 def solicit():
     next_goal = 0
     global client
     global solicit_bool
-
+    global solicit_message
+    global state_label
+    
     while solicit_bool == True:
+        state_label = "Soliciting"
         goal = goal_pose(waypoints[next_goal])
         client.send_goal(goal)
         client.wait_for_result()
-        # print(next_goal, "goal complete")
+        print(solicit_message)
+        rospy.sleep(3)
         if next_goal == 3:
             next_goal = 0
         else:
             next_goal = next_goal + 1
 
+def Solicit_Message(msg):
+    global solicit_message
+    solicit_message = msg.data
+
 def goto(waypoint):
     global client
+    global state_label
+
+    TIME_ALLOWED = 300
+
+    start_time = rospy.get_time()
 
     goal = goal_pose(waypoints[waypoint])
     client.send_goal(goal)
-    client.wait_for_result
+    if waypoint in range(0,3):
+        time_lapse = rospy.get_time() - start_time
+        state_label = "In_customer_delivery"
+        rospy.loginfo(state_label)
+        if time_lapse > TIME_ALLOWED:
+            client.cancel_goal()
+            goto(4)
+            client.wait_for_result()
+            print("Cannot deliver to seating",prev_cmd," for some reason")
+    elif waypoint == 4:
+        state_label = "Going_to_kitchen"
+        rospy.loginfo(state_label)
+    elif waypoint ==  5:
+        state_label = "Going_to_charging"
+        rospy.loginfo(state_label)
+
+def state_update():
+    #0 pending, 1 active, 2 preempted, 3 succeeded, 4 aborted
+    #5 rejected, 6 preempting, 7 recalling, 8 recalled, 9 lost
+    global client
+    global state_label
+    state = client.get_state()
+    if state == 3  and state_label == "Going_to_kitchen":
+        state_label = "At_kitchen"     
+    elif state == 3 and state_label == "Going_to_charging":
+        state_label = "At_charging"
+    elif state == 9 or state_label == None:
+        state_label = "N/A. Send navigation command before state retrieval"
+    elif state == 3 and state_label == "In_customer_delivery":
+        print("Enjoy your meal!")
+        rospy.sleep(2)
+        goto(5)
+        
+
+# def confirmation(): #for remote confirmation
+#     global prev_cmd
+#     global confirm_bool
+#     # print("state_label", state_label, " confirm_bool", confirm_bool)
+#     if state_label == "At_kitchen" and confirm_bool == True:
+#         print("confirmation prompt sent")
+#         confirm_pub.publish(prev_cmd)
+#         confirm_bool = False
+
+def local_confirmation():
+    global prev_cmd
+    global state_label
+
+
+    if state_label == "At_kitchen" and prev_cmd in 'ABCD':
+        if not change_bool:
+            print("Place order onto Robot for table ", prev_cmd)
+        else: 
+            print("Order changed. Please give me the new ordered meal")
+        cmd = input ("Enter 'y' to confirm: ")
+        if cmd.lower() == 'y':
+            if prev_cmd == 'A':
+                state_label = "In_customer_delivery"
+                rospy.loginfo("Delivering to table A")
+                goto(0)
+                client.wait_for_result()
+                
+
+            elif prev_cmd == 'B':
+                state_label = "In_customer_delivery"
+                rospy.loginfo("Delivering to table B")
+                goto(1)
+                client.wait_for_result()
+
+            elif prev_cmd == 'C':
+                state_label = "In_customer_delivery"
+                rospy.loginfo("Delivering to table C")
+                goto(2)
+                client.wait_for_result()
+
+            elif prev_cmd == 'D':
+                state_label = "In_customer_delivery"
+                rospy.loginfo("Delivering to table D")
+                goto(3)
+                client.wait_for_result()
+
 
 def actionFollow(msg):
     global solicit_bool
+    # global confirm_bool
     global client
     global waypoints
+    global state_label
+    global prev_cmd
+    global change_bool
 
-    if msg.data == 'a':
-        print("Going to A")
-        goto(0)
-        print("Enjoy your meal.")
-        goto(4)
-        # pub.publish("kitchen")
-        goal = goal_pose(waypoints[4])
-        client.send_goal(goal)
-        print("Returning to Kitchen")
-        client.wait_for_result()
-        # pub.publish("kitchen")
-    elif msg.data == 'b':
-        goal = goal_pose(waypoints[1])
-        client.send_goal(goal)
-        print("Going to B")
-        client.wait_for_result()
-        print("Enjoy your meal.")
-        goal = goal_pose(waypoints[4])
-        client.send_goal(goal)
-        print("Returning to Kitchen")
-        client.wait_for_result()
-        # pub.publish("kitchen")
-    elif msg.data == 'c':
-        print("Going to C")
-        goal = goal_pose(waypoints[2])
-        client.send_goal(goal)
-        client.wait_for_result()
-        # pub.publish("kitchen")
-    elif msg.data == 'd':
-        print("Going to D")
-        goal = goal_pose(waypoints[3])
-        client.send_goal(goal)
-        client.wait_for_result()
-        # pub.publish("kitchen")
+
+    if msg.data in 'ABCD':
+        # confirm_bool = True 
+        prev_cmd = msg.data
+        goto(4)       
+
     elif msg.data == 'cancel':
+        state_label = "Going_to_kitchen"
         solicit_bool = False
         client.cancel_goal()
-        print("Order cancelled, returning to Kitchen")
-        goal = goal_pose(waypoints[4])
+        prev_cmd = msg.data
+        print("Returning to Kitchen")
+        goto(4)
+        client.wait_for_result()
+        print("Ordered cancelled")
+
     elif msg.data == 'change':
-        print("Changing order")
+        change_bool = True
+        rospy.loginfo("Changing order")
         client.cancel_goal()
-        goal = goal_pose(waypoints[4])
+        goto(4)
+        client.wait_for_result()
+        
     elif msg.data == 'solicit':
         print("Soliciting")
         solicit_bool = True
+
+    elif msg.data == 'state':
+        print(state_label, prev_cmd, client.get_state())
+        state_pub.publish(state_label)
+
+    elif msg.data == 'start':
+        main_pub.publish("start")
+        print("Robot connected")
+
 
 
 if __name__ == '__main__':
 
     rospy.init_node('patrol')
-    print("patrol node initiated")
+    print("Robot initiated")
     rospy.Subscriber("commands", String, actionFollow)
+    rospy.Subscriber("solicit_message", String, Solicit_Message)
 
-    
-
-    confirm_pub = rospy.Publisher("state", String, queue_size=1)
+    main_pub = rospy.Publisher("main_menu", String, queue_size=1)
+    state_pub = rospy.Publisher("state", String, queue_size=1)
+    # confirm_pub = rospy.Publisher("confirm", String, queue_size=1)
 
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
 
-    rospy.spin()
+    while not rospy.is_shutdown():
+        solicit()
+        state_update()
+        local_confirmation()
+        # confirmation()      
+    rospy.spin() 
